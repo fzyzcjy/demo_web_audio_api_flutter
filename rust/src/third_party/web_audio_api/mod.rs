eda1e602dbb2aadb8c4c301273ac4655afc1b197
fzyzcjy <ch271828n@outlook.com>
2024-06-03 13:07:09 +0800

    feat: more

--- rust/src/third_party/web_audio_api/mod.rs
+++ rust/src/third_party/web_audio_api/mod.rs
@@ -2,10 +2,26 @@ pub mod worklet;
 pub mod node;
 
 use flutter_rust_bridge::frb;
-use web_audio_api::AudioRenderCapacity;
+use web_audio_api::{AudioBuffer, AudioRenderCapacity};
 
 #[frb(external)]
 impl AudioRenderCapacity {
     #[frb(ignore)]
     pub fn set_onupdate() {}
+}
+
+#[frb(external)]
+impl AudioBuffer {
+    #[frb(ignore)]
+    pub fn copy_from_channel() {}
+    #[frb(ignore)]
+    pub fn copy_from_channel_with_offset() {}
+    #[frb(ignore)]
+    pub fn copy_to_channel() {}
+    #[frb(ignore)]
+    pub fn copy_to_channel_with_offset() {}
+    #[frb(ignore)]
+    pub fn get_channel_data() {}
+    #[frb(ignore)]
+    pub fn get_channel_data_mut() {}
 }
