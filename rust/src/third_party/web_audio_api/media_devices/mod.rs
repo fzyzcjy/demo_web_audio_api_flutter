1cd3f479da8fe1b1f083f75b222009697a90b3e7
fzyzcjy <ch271828n@outlook.com>
2024-06-03 14:30:41 +0800

    feat: ignore more

--- rust/src/third_party/web_audio_api/media_devices/mod.rs
+++ rust/src/third_party/web_audio_api/media_devices/mod.rs
@@ -1,8 +1,10 @@
 use flutter_rust_bridge::frb;
-use web_audio_api::media_devices::MediaDeviceInfo;
 
-#[frb(external)]
-impl MediaDeviceInfo {
-    #[frb(ignore)]
-    fn group_id() {}
-}
+#[frb(ignore)]
+pub struct MediaDeviceInfo;
+
+// #[frb(external)]
+// impl MediaDeviceInfo {
+//     #[frb(ignore)]
+//     fn group_id() {}
+// }
