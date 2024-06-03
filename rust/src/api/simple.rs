3576e9a08f6aa5efec2b11f4cdfe1d498858b6af
fzyzcjy <ch271828n@outlook.com>
2024-06-03 14:26:15 +0800

    chore: simp

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -1,13 +1,6 @@
 pub use std::any::Any;
 pub use std::error::Error;
 
-pub fn dummy_function(
-    a: Box<dyn Any + Send>,
-    b: Box<dyn Error>,
-) {
-    let _ = (a, b);
-}
-
 #[flutter_rust_bridge::frb(init)]
 pub fn init_app() {
     // Default utilities - feel free to customize
