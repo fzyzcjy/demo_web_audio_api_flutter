866a2974bc75f9a16139ff97bf8874f5544541c6
fzyzcjy <ch271828n@outlook.com>
2024-06-03 14:27:09 +0800

    chore: more

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -1,8 +1,14 @@
 pub use std::any::Any;
 pub use std::error::Error;
+use flutter_rust_bridge::frb;
 
-#[flutter_rust_bridge::frb(init)]
+#[frb(init)]
 pub fn init_app() {
     // Default utilities - feel free to customize
     flutter_rust_bridge::setup_default_user_utils();
 }
+
+#[frb(opaque)]
+pub struct DummyStruct;
+
+pub fn f(a: DummyStruct) { let _ = a; }
