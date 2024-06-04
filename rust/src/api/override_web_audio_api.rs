049d2ac38684107fe4a0b4aa5c38443d3933464a
fzyzcjy <ch271828n@outlook.com>
2024-06-04 11:17:38 +0800

    feat: more

--- rust/src/api/override_web_audio_api.rs
+++ rust/src/api/override_web_audio_api.rs
@@ -1,11 +1,13 @@
 use extend::ext;
 use flutter_rust_bridge::for_generated::anyhow;
+use flutter_rust_bridge::frb;
 use web_audio_api::AudioBuffer;
 use web_audio_api::context::{AudioContext, BaseAudioContext};
 
 #[ext]
 impl AudioContext {
-    fn override_decode_audio_data_sync(&self, input_path: String) -> anyhow::Result<AudioBuffer> {
+    #[frb(override)]
+    fn decode_audio_data_sync(&self, input_path: String) -> anyhow::Result<AudioBuffer> {
         let input = std::fs::File::open(input_path)?;
         Ok(self.decode_audio_data_sync(input).map_err(|e| anyhow::anyhow!("{:?}", e))?)
     }
