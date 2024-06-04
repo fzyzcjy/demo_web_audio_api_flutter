15d3b11443b26c42b06f923fb30ef0a02d16f0b2
fzyzcjy <ch271828n@outlook.com>
2024-06-04 12:49:33 +0800

    fix: err

--- rust/src/api/override_web_audio_api.rs
+++ rust/src/api/override_web_audio_api.rs
@@ -4,7 +4,7 @@ use web_audio_api::AudioBuffer;
 use web_audio_api::context::{AudioContext, BaseAudioContext};
 
 #[ext]
-impl AudioContext {
+pub impl AudioContext {
     fn frb_override_decode_audio_data_sync(&self, input_path: String) -> anyhow::Result<AudioBuffer> {
         let input = std::fs::File::open(input_path)?;
         Ok(self.decode_audio_data_sync(input).map_err(|e| anyhow::anyhow!("{:?}", e))?)
