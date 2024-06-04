deb86afbc0f450c4b6a059e623f559ffa05fe40f
fzyzcjy <ch271828n@outlook.com>
2024-06-04 11:16:28 +0800

    feat: impl

--- rust/src/api/override_web_audio_api.rs
+++ rust/src/api/override_web_audio_api.rs
@@ -6,6 +6,7 @@ use web_audio_api::context::AudioContext;
 #[ext]
 impl AudioContext {
     fn override_decode_audio_data_sync(&self, input_path: String) -> anyhow::Result<AudioBuffer> {
-        TODO
+        let input = std::fs::File::open(input_path)?;
+        Ok(self.0.decode_audio_data_sync(input).map_err(|e| anyhow::anyhow!("{:?}", e))?)
     }
 }
