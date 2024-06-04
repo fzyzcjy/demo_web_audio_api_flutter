182a66a4a738b2b1b56f591857bd5023d0cda3e8
fzyzcjy <ch271828n@outlook.com>
2024-06-04 10:58:35 +0800

    feat: more

--- rust/src/third_party/web_audio_api/context/mod.rs
+++ rust/src/third_party/web_audio_api/context/mod.rs
@@ -14,6 +14,7 @@ impl AudioContext {
     fn resume() {}
 
     fn decode_audio_data_sync(&self, input_path: String) -> anyhow::Result<AudioBuffer> {
-        Ok(self.0.decode_audio_data_sync(std::fs::File::open(input_path)?)?)
+        let input = std::fs::File::open(input_path)?;
+        Ok(self.0.decode_audio_data_sync(input).map_err(|e| anyhow::anyhow!("{:?}", e))?)
     }
 }
